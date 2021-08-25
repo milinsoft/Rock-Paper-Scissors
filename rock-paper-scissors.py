653ee531131d190a664a44efd813d41efe7443de
Aliaksandr Milinkevich <aleksander@Aliaksandrs-MacBook-Air.local>
2021-08-25 23:37:38 +0200

    stage 2/5 at this stage computers choice is random

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,6 @@
+import random
+#random.seed(0)
+
 def taking_user_input():
     # This function takes a user's input, checks if it's a valid, only ["scissors", "paper", 'rock'] options accepted
     # and returns this input if it's valid or starts over otherwise
@@ -8,17 +11,28 @@ def taking_user_input():
     return user_choice
 
 
-def computers_choice(user_choice):
-    # as in this stage of program computer must always win, this function takes user's input value as an argument,
-    # and uses the dict to pick an option for computer that defeats the one picked by the user, and
-    # returns picked option for computer
+def pc_random_choice():
+    # as at 2nd stage of the program computer depict an option randomly - the function no longer takes any arguments and returns random pc choice.
+    pc_choice = random.choice(('rock', 'paper', 'scissors'))
+    return pc_choice
+
+def output_result(user_choice, pc_option):
+    # this function uses combinations dictionary to choose the winner.
+    # "combinations" keys are the options of the game and it's keys represents the option that defeats the key
+    # This function does not return anything, but prints out the result.
     combinations = {'rock':'paper', 'paper':'scissors', 'scissors':'rock'}
-    comp_choice = combinations[user_choice]
-    return comp_choice
+    if combinations[pc_option] == user_choice:
+        print(f"Well done. The computer chose {pc_option} and failed")
+    elif combinations[user_choice] == pc_option:
+        print(f"Sorry, but the computer chose {pc_option}")
+    else:
+        print(f"There is a draw ({pc_option})")
 
 
 if __name__ == "__main__":
     user_choice = taking_user_input()
-    computer = computers_choice(user_choice)
-    print(f"Sorry, but the computer chose {computer}")
+    pc_option = pc_random_choice()
+    output_result(user_choice, pc_option)
+
+
 
